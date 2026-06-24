e48480465cd8073e652229f9af4ea150ede13e16
Deo Muhire <deomuhire@gmail.com>
2026-06-24 10:58:22 +0200

    fix postgres URL for Render

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -8,7 +8,10 @@ const api = axios.create({
   // because all API calls in the app already include `/api/...` paths.
   baseURL: import.meta.env.VITE_API_URL
     ? `${import.meta.env.VITE_API_URL}`
-    : 'http://localhost:5000'
+    : (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
+      ? 'http://localhost:5000'
+      : ''
+
 
 })
 
